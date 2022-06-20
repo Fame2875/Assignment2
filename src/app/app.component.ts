362f23493981ea0806eea9f5b89de143383b119b
FameNew <63010185@KMITL.AC.TH>
2022-06-20 16:34:42 +0700

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Assignment2';
+  userInput:number =0;
+  toggle = false;
+leapYear()
+{
+  this.toggle = !this.toggle;
+  //this.toggle = true;
+  return ((this.userInput % 4 == 0) && ((this.userInput % 100 != 0)) || ((this.userInput % 400 == 0)));
+
 }
+  }
+ 
+
